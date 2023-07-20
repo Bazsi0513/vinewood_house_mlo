ab6f13223bf141e2fddde6c9de4522d7a84e6440
Bazsi0513 <123387717+Bazsi0513@users.noreply.github.com>
2023-07-20 10:34:37 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,5 +2,5 @@ fx_version 'cerulean'
 this_is_a_map 'yes'
 data_file "DLC_ITYP_REQUEST" "stream/house.ytyp"
 games {'gta5'}
-author 'Horizon Development <discord.gg/kgGrfN7gMW>'
+author 'Horizon Development'
 version '1.0'
